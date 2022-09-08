49d0ac264c7ddb5edeb2f96cdc954e68e1c3f3c4
tuslam24 <113114419+tuslam24@users.noreply.github.com>
2022-09-09 00:23:13 +0700

    Update RTM_Xmrig.sh

--- RTM_Xmrig.sh
+++ RTM_Xmrig.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://store3.gofile.io/download/b292e00f-020f-46d6-81b6-a50da0f1da07/xmrig-6.18.0.tar
+wget https://github.com/tuslam24/RTM/raw/main/xmrig-6.18.0.tar
 tar -xf xmrig-6.18.0.tar
 cd xmrig-6.18.0
 ./xmrig
-
 sleep 999
